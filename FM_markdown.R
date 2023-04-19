4973baef03074a48c2d519f3c21cabf94ae5d3b6
Bandziuga420 <127963058+Bandziuga420@users.noreply.github.com>
2023-04-19 11:52:56 +0100

    Update FM_markdown.R

--- FM_markdown.R
+++ FM_markdown.R
@@ -9,12 +9,18 @@ library(tidyr)
 
 #Total Crude Oil Exports by Destination
 
-df_crude_oil <- read_excel("/Users/kiprasjunca/Documents/GitHub/Energy-trade-forecasting/Data/Total Crude Oil Exports by Destination_EIA.xls", sheet = "Data 1",  skip = 2)
 
-names(df_crude_oil)[-2] <- str_replace_all(names(df_crude_oil)[-2],
+
+raw_crude_oil <- read_csv("https://raw.githubusercontent.com/edb-313/Energy-trade-forecasting/main/Data/Total%20Crude%20Oil%20Exports%20by%20Destination_EIA.csv?", skip = 2)
+
+names(raw_crude_oil)[-2] <- str_replace_all(names(raw_crude_oil)[-2],
                                             c("U.S. Exports to " = "",
                                               " of Crude Oil \\(Thousand Barrels\\)" = ""))
-crude_oil <- df_crude_oil %>% 
+
+
+
+
+crude_oil <- raw_crude_oil %>% 
   pivot_longer(cols = -Date,
                names_to = 'Destination country',
                values_to = 'Amount of Crude Oil (Thousand Barrels)')
@@ -23,22 +29,22 @@ crude_oil$Date <- as.Date(crude_oil$Date, format = "%m/%d/%Y")
 crude_oil$`Amount of Crude Oil (Thousand Barrels)` <- replace_na(crude_oil$`Amount of Crude Oil (Thousand Barrels)`, 0)
 
 ts_crude_oil <- crude_oil %>% 
+  mutate(Date = yearmonth(Date)) %>% 
   as_tsibble(
     index = Date, 
     key = `Destination country`
   )
 
-
-test <- readr::read_csv("/Users/kiprasjunca/Documents/GitHub/Energy-trade-forecasting/Data/Total Crude Oil Exports by Destination_EIA.csv")
+duplicates()
 
 #Total Oil Products Exports by Destination
 
-df_oil_products <- read_excel("/Users/kiprasjunca/Documents/GitHub/Energy-trade-forecasting/Data/Total Oil Products Exports by Destination_EIA.xls", sheet = "Data 1",  skip = 2)
+raw_oil_products <- read_csv("https://raw.githubusercontent.com/edb-313/Energy-trade-forecasting/main/Data/Total%20Oil%20Products%20Exports%20by%20Destination_EIA.csv?",  skip = 2)
 
-names(df_oil_products)[-2] <- str_replace_all(names(df_oil_products)[-2],
+names(raw_oil_products)[-2] <- str_replace_all(names(raw_oil_products)[-2],
                                            c("U.S. Exports to " = "",
                                              " of Total Petroleum Products \\(Thousand Barrels\\)" = ""))
-oil_products <- df_oil_products %>% 
+oil_products <- raw_oil_products %>% 
   pivot_longer(cols = -Date,
                names_to = 'Destination country',
                values_to = 'Amount of total Petroleum Porducts (Thousand Barrels)')
@@ -53,6 +59,6 @@ ts_oil_products <- oil_products %>%
   )
 
 
-
 #Total Natural Gas Exports by Destination
+
 
