cd3f038a4d9e2a25bf4f43d80149d64d6bce280b
tomi0227 <128819225+tomi0227@users.noreply.github.com>
2023-04-20 11:58:07 +0100

    Code for converting natural gas file

---
+++ R code for natural gas exports.R
@@ -0,0 +1,48 @@
+library(fpp3)
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(dplyr)
+
+
+
+Nat_gas <- read.csv('https://raw.githubusercontent.com/edb-313/Energy-trade-forecasting/main/Data/csv/Natural%20Gas%20Exports%20by%20Country_EIA.csv', header=TRUE , check.names=FALSE,skip = 2)
+
+# drop columns 2-3, 6-8, 54 and 57-72
+Nat_gas <- subset(Nat_gas, select = -c(2:3, 6:8,54, 57:72))
+Nat_gas[is.na(Nat_gas)] <- 0
+#cimbining exports to the same countries
+Nat_gas <- Nat_gas %>%
+  mutate(Mexico = `U.S. Natural Gas Pipeline Exports to Mexico (MMcf)` + `Liquefied U.S. Natural Gas Exports by Truck to Mexico (Million Cubic Feet)`,
+         Canada = `U.S. Natural Gas Pipeline Exports to Canada (MMcf)` + `Liquefied U.S. Natural Gas Exports by Truck to Canada (Million Cubic Feet)`)
+#removing duplicate columns
+Nat_gas <- Nat_gas[, !(names(Nat_gas) %in% c('U.S. Natural Gas Pipeline Exports to Mexico (MMcf)',
+                                                'Liquefied U.S. Natural Gas Exports by Truck to Mexico (Million Cubic Feet)',
+                                                'U.S. Natural Gas Pipeline Exports to Canada (MMcf)',
+                                                'Liquefied U.S. Natural Gas Exports by Truck to Canada (Million Cubic Feet)'))]
+
+#cleaning column names
+colnames(Nat_gas) <- gsub(pattern = ".*(to\\s)", "", colnames(Nat_gas))
+colnames(Nat_gas) <- gsub("\\(.*?\\)", "", colnames(Nat_gas))
+
+#pivoting data
+Gastemp <- Nat_gas %>% 
+  pivot_longer(cols = -Date,
+               names_to = 'Destination country',
+               values_to = 'Amount of Natural gas (MMcf)')
+
+Gastemp$Date <- as.Date(Gastemp$Date, format = "%d/%m/%Y")
+
+sum(is.na(Gastemp$Date))
+
+Gastemp <- Gastemp[complete.cases(Gastemp$Date), ]
+
+#converting to tsibbles
+ts_natgas <- Gastemp %>% 
+  as_tsibble(
+    index = Date, 
+    key = `Destination country`
+  )
